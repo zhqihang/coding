2f248752ff689af0f1302589ac7bdc78c9e45a91
zhqihang <qihang9903@163.com>
2024-07-08 10:27:08 +0800

    2024-07-08

---
+++ src/leetcode/hot100/Demo_124.java
@@ -0,0 +1,45 @@
+package leetcode.hot100;
+
+/**
+ * @Author: zhqihang
+ * @Date: 2024/07/08
+ * @Project: coding
+ * @Description: 二叉树的最大路径和
+ *
+ * 动态规划
+ */
+public class Demo_124 {
+    public static int ans = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+        int lVal = dfs(node.left); // 左子树最大链和
+        int rVal = dfs(node.right); // 右子树最大链和
+        ans = Math.max(ans, lVal + rVal + node.val); // 两条链拼成路径
+        return Math.max(Math.max(lVal, rVal) + node.val, 0); // 当前子树
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
