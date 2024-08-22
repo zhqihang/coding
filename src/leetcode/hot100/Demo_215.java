b2653b72e45d22ac34fa4ddb47ab49c84786044a
zhqihang <qihang9903@163.com>
2024-08-22 13:56:41 +0800

    2024-08-22

--- src/leetcode/hot100/Demo_215.java
+++ src/leetcode/hot100/Demo_215.java
@@ -1,5 +1,9 @@
 package leetcode.hot100;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * @description: 数组中的第k个最大元素
  * @author: zhqihang
@@ -13,6 +17,34 @@ public class Demo_215 {
 
     // 无序数组  时间复杂度O(n) 所以不能排序再找
     public int findKthLargest(int[] nums, int k) {
-        return 0;
+        List<Integer> numList = new ArrayList<>();
+        for (int num : nums) {
+            numList.add(num);
+        }
+        return quickSelect(numList, k);
+    }
+
+     // 快速选择算法
+    private int quickSelect(List<Integer> nums, int k) {
+        // 随机选择基准数
+        Random random = new Random();
+        int pivot = nums.get(random.nextInt(nums.size())); // 基准数
+
+        // 根据基准数划分将数组元素三个部分
+        List<Integer> big = new ArrayList<>();
+        List<Integer> equal = new ArrayList<>();
+        List<Integer> small = new ArrayList<>();
+        for (Integer num : nums) {
+            if (num > pivot) big.add(num);
+            else if (num == pivot) equal.add(num);
+            else small.add(num);
+        }
+        // 判断第k大的元素在哪个集合
+        if (k <= big.size())
+            return quickSelect(big, k); // 答案在大于部分
+        if (k > nums.size() - small.size())
+            // 答案在小于部分（延伸出了 equal & big）
+            return quickSelect(small, k - nums.size() + small.size());
+        return pivot; // 递归出口
     }
 }
